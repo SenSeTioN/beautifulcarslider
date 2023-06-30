6f1d3957c522b2167821b2b1c03ea743f4488ea2
SenSeTioN <lankovedik@gmail.com>
2023-06-30 18:33:28 +0300

    Внес небольшие правки

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlugin(activeSlide = 2) {
+	const slides = document.querySelectorAll('.slide');
 
-const clearActiveClasses = () => {
+	slides[activeSlide].classList.add('active');
+
+	const clearActiveClasses = () => {
+		slides.forEach(slide => {
+			slide.classList.remove('active');
+		});
+	};
+
 	slides.forEach(slide => {
-		slide.classList.remove('active');
+		slide.addEventListener('click', () => {
+			clearActiveClasses();
+			slide.classList.add('active');
+		});
 	});
-};
-
-slides.forEach(slide => {
-	slide.addEventListener('click', () => {
-		clearActiveClasses();
-
-		slide.classList.add('active');
-	});
-});
+}
